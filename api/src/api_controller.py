0fa9d51ba8f4ed169412bd6b03c007a7c12c893d
iallende <i.allende@netconomy.net>
2019-03-30 08:54:11 +0100

    now health check does something

--- api/src/api_controller.py
+++ api/src/api_controller.py
@@ -1,12 +1,27 @@
 import flask
+import sqlite3
+
+from flask_api import status
+from sqlite3 import Error
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+DB_CONN = None
 
 @app.route('/api/v1/health', methods=['GET'])
 def healtch_check():
-    return "<h1>This will eventually tell you if system is running</h1>"
+    """ Check if db is up and running """
+    if (not _create_connection()):
+        return 'everything is ok', status.HTTP_200_OK
+    return 'ups, there seems to be an error on our side', status.HTTP_503_SERVICE_UNAVAILABLE
+
+def _create_connection():
+    """ create a database connection to a SQLite database """
+    try:
+        DB_CONN = sqlite3.connect('metrics.db')
+    except Error as e:
+        return e
 
 @app.route('/api/v1/metrics/stats', methods=['GET'])
 def fetch_stats():
